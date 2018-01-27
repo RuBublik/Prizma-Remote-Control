0eaed789d665c5157e73eb270df633d37a191f53
RuBublik <relikr@gmail.com>
2018-01-27 10:11:54 +0200

    working print screen send/recv scripts

--- getter.py
+++ getter.py
@@ -11,19 +11,21 @@ print 'waiting for connection to be established'
 getter, addr = s.accept()
 print 'connection established successfully'
 
-data = getter.recv(1000000)
-print type(data), len(data)
+size = pickle.loads(getter.recv(1024))
+scrn = getter.recv(10000000)
+print type(scrn), len(scrn), size
 getter.close()
 '''
 f = open('test1.txt','w')
-f.write(data)
+f.write(scrn)
 f.close()
 print 'f.write done'
 f = open('test1.txt','w')
 '''
-unpkd = pickle.loads(data)
-print type(unpkd)
-im = Image.open(unpkd)#im = Image.open(StringIO.StringIO(data))
+#unpkd = pickle.loads(scrn)
+#print type(unpkd)
+im = Image.frombytes('RGB', size, scrn)
+im.show()#im = Image.open(StringIO.StringIO(scrn))
 raw_input()
 
 
--- sender.py
+++ sender.py
@@ -3,9 +3,13 @@ from PIL import ImageGrab
 
 s = socket.socket()
 s.connect(('127.0.0.1',4444))
+
 im = ImageGrab.grab()
-im.save('scrn.jpg')
-print 'saved'
-d = pickle.dumps(im)#d = t.tobytes()
-s.send(d)
+#im.save('scrn.jpg')
+#print 'saved'
+w, h = im.size
+print (w,h)
+
+s.send(pickle.dumps((w,h)))
+s.send(im.tobytes())
 s.close()
